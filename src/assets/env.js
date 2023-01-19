a169d6e44fd2bed930f56966079659b83e90634b
jv-demed <jv.demed@gmail.com>
2023-01-18 23:06:23 -0300

    Fix: Nova conta DATO

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,7 +1,7 @@
-export const readTokenDato = '09270f8d31397fb81a4b17cf647a73';
-export const masterTokenDato = 'ad3ed88b13a766afa73256efa9ad94';
-export const apiKeyOpenai = '';
-export const questionId = '1147666';
-export const articleId = '1161173';
+//export const readTokenDato = '09270f8d31397fb81a4b17cf647a73';
+export const readTokenDato = '34921ef3d68bf9a5776f5839533c46';
+//export const masterTokenDato = 'ad3ed88b13a766afa73256efa9ad94';
+export const masterTokenDato = 'cf643096d73c54de00253b76f345ee';
+export const articleId = '1218935';
 //export const question = 'Apresente em língua portuguesa uma tabela informando os genes, os polimorfismos associados com hipertensão, a significância p, a nacionalidade da população estudada, o tamanho da amostra e a conclusão sobre os polimorfismos estudados, com o seguinte cabeçalho: PMID | DOI | gene | polimorfismo | p | População | Tamanho amostral | Conclusão';
 export const question = 'Apresente em língua portuguesa os genes, os polimorfismos associados com hipertensão, a significância p, a nacionalidade da população estudada, o tamanho da amostra e a conclusão sobre os polimorfismos estudados, organizado da seguinte forma: PMID|DOI|gene|polimorfismo|p|população|tamanho amostral|conclusão.';
